79a389248832d2d42358ed2df778be4497c69118
Ashish Ranjan <ashish.r@browserstack.com>
2021-06-06 07:04:16 +0530

    logo

--- gulpfile.js
+++ gulpfile.js
@@ -4,21 +4,43 @@ const babelify = require('babelify');
 const source = require('vinyl-source-stream');
 const buffer = require('vinyl-buffer');
 const uglify = require('gulp-uglify');
+const zip = require('gulp-zip');
+const jeditor = require('gulp-json-editor');
 
-function javascriptBuild() {
+exports.buildJs = gulp.task('buildJs', function () {
   return (
     browserify({
       entries: ['src/script.js', 'src/ui.js'],
       transform: [babelify.configure({ presets: ['@babel/preset-env'] })],
     })
       .bundle()
-      .pipe(source('bundle.js'))
+      .pipe(source('main.js'))
       // Turn it into a buffer!
       .pipe(buffer())
       // And uglify
       .pipe(uglify())
-      .pipe(gulp.dest('bin'))
+      .pipe(gulp.dest('bin/build'))
   );
-}
+});
 
-exports.build = javascriptBuild;
+exports.copyManifest = gulp.task('copyManifest', function () {
+  return gulp
+    .src('./manifest.json')
+    .pipe(
+      jeditor(function (json) {
+        json.content_scripts[0] = { ...json.content_scripts[0], js: ['main.js'] };
+        return json; // must return JSON object.
+      })
+    )
+    .pipe(gulp.dest('bin/build'));
+});
+
+exports.copyLogos = gulp.task('copyLogos', function () {
+  return gulp.src('logo/*').pipe(gulp.dest('bin/build/logo'));
+});
+
+exports.zipAssets = gulp.task('zipAssets', function () {
+  return gulp.src(['bin/build/**']).pipe(zip('cowin-vaccinator.zip')).pipe(gulp.dest('bin'));
+});
+
+gulp.task('build', gulp.series('buildJs', 'copyManifest', 'copyLogos', 'zipAssets'));
